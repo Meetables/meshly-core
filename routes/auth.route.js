768f2310be095605f8a64a096708e43bf002e353
Andreas Molnar <andreas24juni@gmail.com>
2025-03-31 18:35:11 +0200

    completed login, logout and verifyAuthMiddleware functions

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { signup, login, logout } = require('../controllers/auth.controller');
+const verifyAuth = require('../middleware/verifyAuth.middleware');
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ router.post("/login", login)
 
 router.post("/logout", logout)
 
+router.post("/test", verifyAuth, (req, res) => {
+    res.status(200).json({"authorized": "true"})
+})
 
 module.exports = router; 
